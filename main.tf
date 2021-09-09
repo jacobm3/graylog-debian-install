525b0e49b3c67901cba3a6a305716f59f9cd47d2
Jacob <jacobm3@gmail.com>
2021-09-09 00:29:03 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+   tags = {
+     Terraform = true
+     Owner       = "email@domain.com"
+     TTL     = 768
+   }
+ }
+}
+
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.latest-ubuntu.id
+  instance_type = "t3.medium"
+  key_name = "acer-wsl"
+  root_block_device { volume_size = 40 }
+}
+
+output "public_ip" {
+  value       = aws_instance.web.public_ip
+}
