e17e6e7dd6d94fabdb1eb5f9dcc009a1815a7e90
Jacob <jacobm3@gmail.com>
2023-12-02 17:02:54 -0600

    Update graylog-5.2-ubuntu22.sh

--- tmp/graylog-5.2-ubuntu22.sh
+++ tmp/graylog-5.2-ubuntu22.sh
@@ -61,7 +61,8 @@ sudo systemctl status --no-pager opensearch.service
 # graylog
 wget https://packages.graylog2.org/repo/packages/graylog-5.2-repository_latest.deb
 sudo dpkg -i graylog-5.2-repository_latest.deb
-sudo apt-get update && sudo apt-get install graylog-server=5.2.1-1 
+# sudo apt-get update && sudo apt-get install graylog-server=5.2.1-1 
+sudo apt-get update && sudo apt-get install graylog-server
 
 sudo cp /etc/graylog/server/server.conf /etc/graylog/server/server.conf.dist
 # SEC=$(< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c${1:-96} | head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1)
