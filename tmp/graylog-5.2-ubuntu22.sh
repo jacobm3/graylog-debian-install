69a1cfada06cde14a4427110764835b71c5b2324
Jacob <jacobm3@gmail.com>
2023-12-02 16:10:12 -0600

    Update graylog-5.2-ubuntu22.sh

--- tmp/graylog-5.2-ubuntu22.sh
+++ tmp/graylog-5.2-ubuntu22.sh
@@ -10,6 +10,16 @@ sudo timedatectl set-timezone UTC
 
 # mongo
 wget -qO- 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xf5679a222c647c87527c2f8cb00a0bd1e2c63c11' | sudo apt-key add -
+
+# https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+sudo apt-get install gnupg curl
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
 sudo systemctl daemon-reload
 sudo systemctl enable mongod.service
 sudo systemctl restart mongod.service
